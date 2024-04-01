b4ffe9f903b2ad70d40d95a0ce21c643d80c497a
IgorMarquezani <igormarquezani@gmail.com>
2024-04-01 14:35:37 -0300

    updating manifest.pp

--- manifest.pp
+++ manifest.pp
@@ -17,7 +17,7 @@ user { 'ixm':
 
 # adicionando o repositório do Google Chrome
 exec { 'adicionar_repo_chrome':
-  command => '/usr/bin/wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list',
+  command => '/usr/bin/wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | /usr/bin/apt-key add - && /usr/bin/echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | /usr/bin/tee /etc/apt/sources.list.d/google-chrome.list',
   require => Package['wget'],
 }
 
